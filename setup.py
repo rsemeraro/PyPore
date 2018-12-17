92a3efd7f7129efbd8f7dff167a4e1d6c29c292b
Roberto Semeraro <robe.semeraro@gmail.com>
2018-12-17 14:10:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-reqs = ['mpi4py==3.0.0', 'numpy==1.15.0', 'h5py==2.8.0', 'h5py_cache==1.0', 'plotly==2.7.0', 'python_dateutil==2.7.3', 'ntpath', 'biopython']
+reqs = ['numpy==1.15.0', 'h5py==2.8.0', 'plotly==2.7.0', 'python_dateutil==2.7.3', 'ntpath', 'biopython']
 
 if os.name != 'nt':
     reqs = reqs + ['pysam==0.13']
